937d719560db59146c14a186f71c3b9c8a802160
Alexandria <alexandria.creek@gmail.com>
2020-09-01 16:25:16 -0700

    add

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ console.log('js is loaded');
 var ready = confirm('wanna play?')
 var answer = null;
  if ( answer === ready ) {
-    answer = prompt ('What is my favorite color?')   
- }else{
+     prompt ('What is my favorite color?')   
+ }else(answer === null) {
      alert("fine...be that way")
  }
