0e9683bd5cf16580761d51a58d038fed3e8bd59a
Alexandria <alexandria.creek@gmail.com>
2020-09-01 16:57:43 -0700

    add

--- app.js
+++ app.js
@@ -6,6 +6,4 @@ var ready = confirm('wanna play?')
 var answer = null;
  if ( answer === true ) {
      answer = prompt ('What is my favorite color?')   
- }else ( answer === false) {
-     alert("fine...be that way")
  }
