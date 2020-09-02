9dd17a596c39af530491046dcb53d3da78358356
Alexandria <alexandria.creek@gmail.com>
2020-09-01 17:05:14 -0700

    add

--- app.js
+++ app.js
@@ -2,4 +2,11 @@
 
 console.log('js is loaded');
 
-var ready = prompt('wanna play a guessing game?')
+var ready = prompt('wanna play a guessing game?');
+var answer1 = null;
+if (ready === true){
+    answer1 = prompt('what is my favorite color?')
+
+} if (answer1 = 'blue') {
+    alert(answer1 + " meh.")
+}
