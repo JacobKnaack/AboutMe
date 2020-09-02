87f3f005d634f0ce7ec64b7d1223faf303e652aa
Alexandria <alexandria.creek@gmail.com>
2020-09-01 21:32:15 -0700

    add

--- app.js
+++ app.js
@@ -2,9 +2,20 @@
 
 console.log('js is loaded');
 
-var ready = prompt('wanna play a guessing game?');
+// Prompt the user
+var ready = prompt('wanna play a guessing game? Y/N');
 var answer1 = null;
-if (ready === true){
-    ready = prompt('what is my favorite color?')
+var answer2 = null;
+var answer3 = null;
+var answer4 = null;
+var answer5 = null;
+
+if (ready === 'Y'){
+answer1 = prompt('Do you like apples?')
+answer2 = prompt('Do you like dogs?')
+answer3 = prompt('Do you like chocolate milk?')
+answer4 = prompt('Do you long walks on the beach?')
+answer5 = prompt('Do you like games?')
+
 
 } 
